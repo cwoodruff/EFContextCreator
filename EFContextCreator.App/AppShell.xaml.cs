09398b4712fd5701066dc2e04f9b74f391e23e69
Chris Woodruff <cwoodruff@live.com>
2022-10-12 22:16:14 -0400

    Initial MAUI project created

---
+++ EFContextCreator.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EFContextCreator.App;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ EFContextCreator.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EFContextCreator.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
